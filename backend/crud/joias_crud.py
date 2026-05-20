6b2f22a24fb9661db4c7709ed2ef0d9973278065
mikhelli <raianymikhelli@gmail.com>
2026-05-19 22:32:26 -0300

    Edição do Crud e criação das funções em categoria e joia

---
+++ backend/crud/joias_crud.py
@@ -0,0 +1,43 @@
+from sqlalchemy.orm import Session
+from models import JoiaDB 
+from schemas import JoiaCreate, JoiaResponse
+
+def criar_joia(db: Session, dados: JoiaCreate): 
+    joia = JoiaDB(**dados.model_dump())
+    db.add(joia)
+    db.commit()
+    db.refresh(joia)
+    return joia
+
+def listar_joias(db: Session):
+    return db.query(JoiaDB).all()
+
+def buscar_joia(db: Session, joia_id: int):
+    return db.query(JoiaDB).filter(JoiaDB.id == joia_id).first()
+
+def atualizar_joia(db: Session, joia_id: int, joia: JoiaCreate):
+    db_joia = buscar_joia(db, joia_id)
+    if db_joia:
+        db_joia.nome = joia.nome
+        db_joia.preco = joia.preco
+        db_joia.categoria_id = joia.categoria_id
+        db.commit()
+        db.refresh(db_joia)
+    return db_joia
+
+def substituir_joia(db: Session, joia_id: int, joia: JoiaCreate):
+    db_joia = buscar_joia(db, joia_id)
+    if db_joia:
+        db_joia.nome = joia.nome
+        db_joia.preco = joia.preco
+        db_joia.categoria_id = joia.categoria_id
+        db.commit()
+        db.refresh(db_joia)
+    return db_joia
+
+def deletar_joia(db: Session, joia_id: int):
+    db_joia = buscar_joia(db, joia_id)
+    if db_joia:
+        db.delete(db_joia)
+        db.commit()
+    return db_joia
