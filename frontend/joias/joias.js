76a7b7d69e53da3c923c845dc4b9b7c7d4d35f08
DeborahAdriao <deborahadriao16@gmail.com>
2026-06-30 21:57:02 -0300

    espero q de certo

--- frontend/joias/joias.js
+++ frontend/joias/joias.js
@@ -84,11 +84,19 @@ $(document).ready(function() {
     });
 
     $(document).on('click', '.btn-deletar-joia', function() {
-        idParaDeletar = $(this).data('id'); 
-        const nomeDaJoia = $(this).attr('data-nome');
-        $('#nome-item-excluir').text(`"${nomeDaJoia}"`); 
-        modalExcluir.show();
-    });
+    idParaDeletar = $(this).data('id'); 
+    const nomeDaJoia = $(this).attr('data-nome');
+    
+    console.log("Nome capturado:", nomeDaJoia); 
+    
+    if (nomeDaJoia) {
+        $('#nome-item-excluir').text(`"${nomeDaJoia}"`);
+    } else {
+        $('#nome-item-excluir').text("este item"); 
+    }
+    
+    modalExcluir.show();
+});
 });
 
 async function carregarVitrine() {
